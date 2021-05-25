0b4b1c708db0b067fe4b45b2b08e02902f119e48
gabrisca <gabrielescarparo1985@gmail.com>
2021-05-25 16:39:08 +0200

    creazione form select (da terminare)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,27 +1,20 @@
 <template>
   <div class="container mt-5 text-center">
     <!-- salvo in un div con condizione !loading il mio contenuto -->
-    <div 
-    v-if="!loading"
-    class="row d-flex justify-content-center">
-      <Search 
-      @searchAlbum='searching'
+    <div v-if="!loading" class="row d-flex justify-content-center">
+      <Search @searchAlbum="searching" />
+      <Album
+        v-for="(album, index) in filteredAlbums"
+        :key="index"
+        :album="album"
       />
-      <Album 
-      v-for="(album, index) in filteredAlbums" 
-      :key="index" 
-      :album="album"
-       />
-       <div class="text-center mt-3">
-         <h6>Album trovati: {{ filteredAlbums.length }}</h6>
-       </div>
-       <SelectGenre />
+      <div class="text-center mt-3">
+        <h6>Album trovati: {{ filteredAlbums.length }}</h6>
+      </div>
+      <SelectGenre />
     </div>
     <!-- se loading è true mostro Loader con icona caricamento pagina -->
-      <Loader 
-      v-else
-      title="Spotify"
-      />
+    <Loader v-else title="Spotify" />
   </div>
 </template>
 
@@ -31,11 +24,11 @@ import axios from "axios";
 // importo in Main Album
 import Album from "./Album.vue";
 // importo in Main Loader
-import Loader from './Loader.vue';
+import Loader from "./Loader.vue";
 // importo Search
-import Search from './Search.vue';
+import Search from "./Search.vue";
 // importo SelectGenre
-import SelectGenre from './SelectGenre.vue';
+import SelectGenre from "./SelectGenre.vue";
 
 // dichiaro i componenti importati
 export default {
@@ -52,24 +45,27 @@ export default {
   data() {
     return {
       axios,
-      albums: [],
       loading: true,
+      albums: [],
       // variabile textToSearch che accoglie il testo inviato dal figlio (di default è vuota)
-      textToSearch:'',
-    }
+      textToSearch: "",
+      genres: [],
+    };
   },
   computed: {
     // filtro gli album in base al titolo
-    filteredAlbums(){
-      return this.albums.filter(item => item.author.toLowerCase().includes(this.textToSearch.toLowerCase()))
-    }
+    filteredAlbums() {
+      return this.albums.filter((item) =>
+        item.author.toLowerCase().includes(this.textToSearch.toLowerCase())
+      );
+    },
   },
   methods: {
     // funzione richiamata da evento custom che riceve come parametro il testo da cercare
-    searching(text){
+    searching(text) {
       this.textToSearch = text;
       console.log(text);
-    }
+    },
   },
   created() {
     // chiamata API
@@ -78,7 +74,7 @@ export default {
       .then((res) => {
         this.albums = res.data.response;
         // una volta caricati i dati loading diventa false altrimenti verrebbe visualizzato senza soluzione di continuità
-        this.loading = false
+        this.loading = false;
         console.log(this.albums);
       })
       .catch((error) => {
--- src/components/SelectGenre.vue
+++ src/components/SelectGenre.vue
@@ -1,16 +1,54 @@
 <template>
   <div>
- 
+    <select class="form-select mc_form mt-4" aria-label="Genre selection">
+      <option 
+      v-for="(genre, index) in genresArray"
+      :key="index"
+      selected>{{genre}}</option>
+    </select>
   </div>
 </template>
 
 
 <script>
+// importo in Main axios per la chiamata API
+import axios from "axios";
+
 export default {
   name: 'SelectGenre',
+  data(){
+    return {
+      // creo un array vuoto dove caricare i generi degli album
+      genresArray: ['Select your favorite music',],
+    }
+  },
+  created() {
+    // chiamata API 
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        res.data.response.forEach((album)=> {
+          // forEach su ogni album dell'array
+          // se l'array non contiene il genere dell'album faccio un pudh nell'array genresArray
+          if(!this.genresArray.includes(album.genre)){
+            this.genresArray.push(album.genre)
+          }
+          // .sort sull'array restituisce l'ordine delle stringhe invertito 
+          // mi serve per visualizzare l'option di default 'Select your music'
+          this.genresArray.sort()
+        })
+        console.log(this.genresArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .mc_form {
+    width: 300px;
+    margin: 0 auto;
+  }
 </style>
