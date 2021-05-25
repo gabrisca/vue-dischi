5c8e5db8404dec3d799799c3a89a58b56b63ca47
gabrisca <gabrielescarparo1985@gmail.com>
2021-05-25 13:16:07 +0200

    form search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,13 +4,20 @@
     <div 
     v-if="!loading"
     class="row d-flex justify-content-center">
+      <Search 
+      @searchAlbum='searching'
+      />
       <Album 
-      v-for="(album, index) in albums" 
+      v-for="(album, index) in filteredAlbums" 
       :key="index" 
       :album="album"
        />
+       <div class="text-center mt-3">
+         <h6>Album trovati: {{ filteredAlbums.length }}</h6>
+       </div>
+       <SelectGenre />
     </div>
-    <!-- se loading è true mostro Loader con incola caricamento pagina -->
+    <!-- se loading è true mostro Loader con icona caricamento pagina -->
       <Loader 
       v-else
       title="Spotify"
@@ -25,12 +32,18 @@ import axios from "axios";
 import Album from "./Album.vue";
 // importo in Main Loader
 import Loader from './Loader.vue';
+// importo Search
+import Search from './Search.vue';
+// importo SelectGenre
+import SelectGenre from './SelectGenre.vue';
 
 // dichiaro i componenti importati
 export default {
   components: {
     Album,
     Loader,
+    Search,
+    SelectGenre,
   },
   // nei data inserisco:
   // - axios per la chiamata Api,
@@ -41,7 +54,22 @@ export default {
       axios,
       albums: [],
       loading: true,
-    };
+      // variabile textToSearch che accoglie il testo inviato dal figlio (di default è vuota)
+      textToSearch:'',
+    }
+  },
+  computed: {
+    // filtro gli album in base al titolo
+    filteredAlbums(){
+      return this.albums.filter(item => item.author.toLowerCase().includes(this.textToSearch.toLowerCase()))
+    }
+  },
+  methods: {
+    // funzione richiamata da evento custom che riceve come parametro il testo da cercare
+    searching(text){
+      this.textToSearch = text;
+      console.log(text);
+    }
   },
   created() {
     // chiamata API
---
+++ src/components/Search.vue
@@ -0,0 +1,52 @@
+<template>
+  <form class="d-flex mt-3 mb-3">
+    <div class="flex-grow-1">
+     <input v-model.trim='searchText' @keyup.enter='startSearch' class="form-control" type="text" placeholder="Find author"> 
+    </div>
+    <div class="ms-3">
+      <!-- .prevent evita che si ricarichi la pagina ogni volta che clicco -->
+      <button @click.prevent='startSearch' class="btn btn-primary"> 
+        Cerca
+      </button>
+    </div>
+    <div class="ms-3">
+      <!-- .prevent evita che si ricarichi la pagina ogni volta che clicco -->
+      <button @click.prevent='resetSearch' class="btn btn-warning"> 
+        Reset
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data(){
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    // funzione che fa partire la ricerca
+    startSearch(){
+      // $emit è un evento che viene letto dal padre
+      this.$emit('searchAlbum', this.searchText)
+      console.log(this.searchText);
+    },
+    // funzione che resetta la ricerca
+    resetSearch(){
+      this.searchText = '';
+      this.startSearch();
+    }
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+  input {
+    &:focus {
+      border-color: #3AD65E;
+      box-shadow: inset 0 1px 1px #3ad65e5e, 0 0 4px #3AD65E;
+    }
+  }
+</style>
